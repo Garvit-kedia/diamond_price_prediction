c95156b472cc43168afaa59d667845b68da38e9f
Garvit-kedia <garvitkedia2@gmail.com>
2024-06-11 18:35:32 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+
+
+from setuptools import find_packages,setup
+from typing import List
+
+# HYPEN_E_DOT='-E .
+
+def get_requirments(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        #  IF HYPEN_E_DOT IN requirements:
+        # requirements.remove(HYPEN_E_DOT)
+        # pip install -r requirment .txt
+        
+        return requirements
+
+setup(
+    name="DiamondPricePrediction",
+    version='0.0.1',
+    suthor="gopi",
+    author_email="garvitkedia2@gmail.com",
+    install_requires=get_requirments('requirements.txt'),
+    packages=find_packages()
+    
+)
